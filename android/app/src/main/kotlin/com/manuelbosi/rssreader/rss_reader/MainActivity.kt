0623a939169f332c48db2ebc8b2c3283fe71fbfb
Manuel Bosi <manuel.bosi@webscape.it>
2021-09-26 15:28:25 +0200

    Clean flutter project

---
+++ android/app/src/main/kotlin/com/manuelbosi/rssreader/rss_reader/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.manuelbosi.rssreader.rss_reader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Rss Reader",
+    );
+  }
+}
