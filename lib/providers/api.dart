ab53a5e3bbe2a86f6ff0dca5c4fe16e69a9f6856
Manuel Bosi <manuel.bosi@webscape.it>
2021-09-26 16:22:24 +0200

    getFeeds function to get rss feed from api endpoint

---
+++ lib/providers/api.dart
@@ -0,0 +1,18 @@
+import 'package:rss_reader/models/feed.dart';
+import 'package:webfeed/domain/rss_feed.dart';
+import 'package:http/http.dart' as http;
+import 'package:webfeed/domain/rss_item.dart';
+
+const String feedsUrl =
+    'https://www.mise.gov.it/index.php/it/per-i-media/notizie?format=feed&type=rss';
+
+Future<List<FeedModel>> getFeeds() async {
+  final response = await http.get(Uri.parse(feedsUrl));
+  final RssFeed responseFeeds = RssFeed.parse(response.body);
+
+  List<FeedModel> feedsList = responseFeeds.items!
+      .map((RssItem singleFeed) => FeedModel.fromData(singleFeed))
+      .toList();
+
+  return feedsList;
+}
